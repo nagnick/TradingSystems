e3d51197ab98faaa25172983214bf3e7b6a75efa
Nicolas <nicolasgarcia422@gmail.com>
2023-03-04 21:58:53 -0500

    Clean up of File parser

--- JSONFileParser.h
+++ JSONFileParser.h
@@ -11,18 +11,8 @@ class JSONFileParser{
         std::stringstream buffer;
         buffer << t.rdbuf();
         Poco::JSON::Parser parser;
-        //std::cout<< buffer.str();
         Poco::Dynamic::Var result = parser.parse(buffer.str());
-        // use pointers to avoid copying
         object = result.extract<Poco::JSON::Object::Ptr>();
-        // ex "{ \"test\" : { \"property\" : \"value\" } }";
-        // Poco::Dynamic::Var test = object->get("test"); // holds { "property" : "value" }
-        // Poco::JSON::Object::Ptr subObject = test.extract<Poco::JSON::Object::Ptr>();
-        // test = subObject->get("property");
-        // std::string val = test.toString(); // val holds "value"
-        // copy/convert to Poco::DynamicStruct
-        // Poco::DynamicStruct ds = *object;
-        // val = ds["test"]["property"]; // val holds "value"
     }
     std::string getValue(std::string key){
         Poco::Dynamic::Var test = object->get(key);
@@ -35,6 +25,6 @@ class JSONFileParser{
         return test.toString();
     }
     ~JSONFileParser(){
-        
+
     }
 };
