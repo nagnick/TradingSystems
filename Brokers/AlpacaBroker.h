dea746dac281d6b589be358c5e8df9e4d1c6e4de
Nicolas <nicolasgarcia422@gmail.com>
2023-03-09 11:09:39 -0500

    clean up of class

--- Brokers/AlpacaBroker.h
+++ Brokers/AlpacaBroker.h
@@ -43,7 +43,6 @@ class AlpacaBroker: public IBroker{ // can trade stocks and crypto. no options
         char* text = new char[length];
         s.getline(text,length);
         std::string res(text);
-        //std::cout << text;
         delete[] text;
         return res;
     };
@@ -63,7 +62,6 @@ class AlpacaBroker: public IBroker{ // can trade stocks and crypto. no options
         std::istream& s = session->receiveResponse(response);
         std::cout << response.getStatus() << " " << response.getReason() << response.getKeepAlive()<< std::endl;
         int length = response.getContentLength();
-    //std::cout << length << std::endl;
         std::stringstream buffer;
         buffer << s.rdbuf();
         Poco::JSON::Parser parser;
@@ -86,12 +84,12 @@ class AlpacaBroker: public IBroker{ // can trade stocks and crypto. no options
             if(price != "NULL" && price != ""){
                 obj.set("limit_price",price);
             }
-            if(stop != "NULL" && price != ""){
+            if(stop != "NULL" && stop != ""){
                 obj.set("stop_price",stop);
             }
             //set("trail_percent","NULL").set("trail_price","NULL");  might have to add these fields later for more complex orders
             Poco::JSON::Object::Ptr result = sendRequestAndReturnJSONResponse("POST", "/v2/orders",obj);
-            std::cout << result->get("id").toString() << std::endl;
+        //std::cout << result->get("id").toString() << std::endl;
             return OrderResponse(result->get("id").toString(),result->get("status").toString());
     }
     void placeOrder(string symbol, string qty, string notional, string side, string type,
