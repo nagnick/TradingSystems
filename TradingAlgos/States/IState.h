e4709fee9a0673f98bae7a95ae1dd292293104a2
Nicolas <nicolasgarcia422@gmail.com>
2023-04-07 13:10:50 -0400

    Start of State interface for trading algos

--- TradingAlgos/States/IState.h
+++ TradingAlgos/States/IState.h
@@ -1,4 +1,13 @@
 #pragma once
-class IState{
+#include "Streams/StreamData.h"
 
+#include <memory>
+
+class IState{
+    // dragged over from the IDataStreamSubscriber duplicate code but want to avoid dragging anything unnecessary from the IDataStreamSub as it grows out
+    // only want the onData methods
+    virtual void onData(std::shared_ptr<IStreamData> data) = 0; // defualt case
+    virtual void onData(std::shared_ptr<OtherData> other) = 0;
+    virtual void onData(std::shared_ptr<TradeData> trade) = 0;
+    virtual void onData(std::shared_ptr<QuoteData> quote) = 0;
 };
