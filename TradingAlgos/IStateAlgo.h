6382af1d8ee1a3ffa8dec2ee31ee179345176c64
Nicolas <nicolasgarcia422@gmail.com>
2023-04-10 17:14:19 -0400

    add fields to hold state and delete at end

--- TradingAlgos/IStateAlgo.h
+++ TradingAlgos/IStateAlgo.h
@@ -7,7 +7,17 @@
 class IStateAlgo: public QueuedStreamSubscriber{
     IState* current = nullptr;
     public:
-    virtual ~IStateAlgo(){};
+    IState* buy = nullptr;
+    IState* sell = nullptr;
+    virtual ~IStateAlgo(){
+        if( buy != current){
+            delete buy;
+        }
+        if(sell != current){
+            delete sell;
+        }
+        delete current;
+    };
     // these are called by the QueuedStreamSub when processing data. With this data let the current state decide next action
     virtual void onData(std::shared_ptr<IStreamData> data){
         current->onData(data);
@@ -22,8 +32,10 @@ class IStateAlgo: public QueuedStreamSubscriber{
         current->onData(quote);
     };
     virtual void swapState(IState* newState){
-        IState* temp = current;
+        if( buy != current && sell != current){
+            delete current;
+        }
+
         current = newState;
-        delete temp;
-    }
+    };
 };
