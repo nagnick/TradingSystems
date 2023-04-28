f23c100adeb39eff601d35f6bbcd7257e8e8d810
Nicolas <nicolasgarcia422@gmail.com>
2023-04-28 00:44:58 -0400

    copy state algo into single class simpleAlgo

--- TradingAlgos/SimpleAlgo.h
+++ TradingAlgos/SimpleAlgo.h
@@ -1,35 +1,31 @@
 #pragma once
-#include "Streams/IDataStreamSubscriber.h"
-#include "Brokers/IBroker.h"
+#include "Factories/ISystemComponentFactory.h"
 
 #include "vector"
 class SimpleAlgo: public IDataStreamSubscriber {
-    IBroker& broker;
-    IDataStream& stream;
-    double open, close, high, low, volume;
-    double buyingPower; // tends to include margin....
-    double entryPrice = 0;
-    double exitPrice = 1000;
-    bool inPosition = false;
+    ISystemComponentFactory& factory;
+    std::string symbol;
+    bool paper;
+    std::string orderId;
+    double lastPrice;
+    enum class States{BUY,SELL,PENDING};
+    States currentState = States::BUY;
+    enum class Direction{UP,DOWN};
+    Direction currentDirtection = Direction::UP;
     public:
-    SimpleAlgo(IBroker& _broker, IDataStream& _stream):broker(_broker),stream(_stream){
-        stream.subscribe(this);
-        stream.subscribeToDataStream("SPY",this);
-        std::vector<BarResponse> bars = broker.getDailyHistoricalBars("SPY","","");
-        buyingPower = std::stod(broker.getBalance().buying_power);
-        //std::cout << "Buying power" << buyingPower << std::endl;
+    SimpleAlgo(ISystemComponentFactory& _factory, std::string _symbol, bool _paper):factory(_factory), symbol(_symbol), paper(_paper){
+        IDataStream* stream = factory.getStream();
+        stream->subscribe(this);
+        stream->subscribeToDataStream(_symbol,this);
+        std::vector<BarResponse> bars = factory.getBroker(paper)->getDailyHistoricalBars(_symbol,"","");
         if(bars.size() > 0){ // initialize the strats market stats...
             BarResponse bar = bars.at(bars.size()-1);
-            open = std::stod(bar.open);
-            close = std::stod(bar.close);
-            high = std::stod(bar.high);
-            low = std::stod(bar.low);
-            volume = std::stod(bar.volume);
+            lastPrice = std::stod(bar.open);
+            // close = std::stod(bar.close);
+            // high = std::stod(bar.high);
+            // low = std::stod(bar.low);
+            // volume = std::stod(bar.volume);
         }
-        // std::cout << "size of bars:" << bars.size() << std::endl;
-        // for (auto &&bar : bars){
-        //     std::cout << "time stamp" << bar.timestamp << std::endl;
-        // }
     };
     virtual void onData(std::shared_ptr<IStreamData> data){
         // default case do nothing with is fall through data
@@ -41,26 +37,63 @@ class SimpleAlgo: public IDataStreamSubscriber {
         // do nothing
     }
     virtual void onData(std::shared_ptr<QuoteData> quote){
-        // std::cout << " Quote ";
-        // std::cout << quote->symbol << quote->bidPrice << quote->bidSize << quote->bidExchange << quote->bidTime << std::endl;
-        // std::cout << quote->askPrice << quote->askSize << quote->askExchange << quote->askTime <<std::endl;
-        double priceIcanBuyAt = std::stod(quote->askPrice); // ask price = lowest a seller will sell at
-        double priceIcanSellAt = std::stod(quote->bidPrice); // bid price = highest a buyer will buy at
-        if(!inPosition && priceIcanBuyAt < open && priceIcanBuyAt < exitPrice){
-            //buy
-            entryPrice = priceIcanBuyAt;
-            OrderResponse res = broker.placeEquityOrder("SPY","buy","10","market","day","","");
-            std::cout << res.status << res.id << std::endl;
-            inPosition = true;
-            std::cout << "Bought 10 shares of spy" << std::endl;
-        }
-        if(inPosition && priceIcanSellAt > entryPrice){
-            // sell
-            exitPrice = priceIcanSellAt;
-            OrderResponse res = broker.placeEquityOrder("SPY","sell","10","market","day","","");
-            std::cout << res.status << res.id << std::endl;
-            inPosition = false;
-            std::cout << "Sold 10 shares of spy" << std::endl;
+        double nextPrice = std::stod(quote->askPrice);
+        switch(currentState){
+            case States::BUY:{
+                if(nextPrice - .1 > lastPrice){
+                    if(currentDirtection == Direction::DOWN){ // wasn't moving much but is now on the rise so buy
+                        // buy
+                        OrderResponse order = factory.getBroker(paper)->placeEquityOrder(symbol,"buy","10","market","day","","");
+                        if(order.id != "-1"){
+                            // order was accepted
+                            orderId = order.id;
+                            currentState = States::PENDING;
+                        }
+                        // change state to pending order...
+                    }
+                    currentDirtection = Direction::UP;
+                }
+                else if( nextPrice - .1 < lastPrice){
+                    currentDirtection = Direction::DOWN;
+                }
+                break;
+            }
+            case States::PENDING:{
+                IBroker* broker = factory.getBroker(paper);
+                OrderResponse res = broker->getOrderByOrderId(orderId);
+                if(res.status == "filled"){
+                    currentState = States::SELL;
+                }
+                else if(res.status == "partially_filled" || res.status == "new"){
+                    return;
+                }
+                else{ // order has not yet been filled something went wrong...
+                    res = broker->cancelOrderByOrderId(orderId);
+                    if(res.status == "ok"){ // restart in previous state
+                        currentState = States::BUY;
+                    }
+                }
+                break;
+            }
+            case States::SELL:{
+                if(nextPrice - .1 < lastPrice){
+                    // sell was going up but now going down....
+                    if(currentDirtection == Direction::UP){
+                        OrderResponse order = factory.getBroker(paper)->placeEquityOrder(symbol,"sell","10","market","day","","");
+                        if(order.id != "-1"){
+                            // order was accepted
+                            orderId = order.id;
+                            currentState = States::BUY;
+                        // change state to pending order...
+                        }
+                    }
+                    currentDirtection = Direction::DOWN;
+                }
+                else if( nextPrice - .1 > lastPrice){
+                    currentDirtection = Direction::UP;
+                }
+                break;
+            }
         }
     }
     ~SimpleAlgo(){};
