023806e0ea40c247f16492ee44a6d29f434c4708
Nicolas <nicolasgarcia422@gmail.com>
2023-04-28 23:08:00 -0400

    Simplification of trail stop state

--- TradingAlgos/States/TrailingStopSellState.h
+++ TradingAlgos/States/TrailingStopSellState.h
@@ -12,36 +12,25 @@ class TrailingStopState: public IState{ // my own implementation of trailing sto
     bool percentStop;
     double stop;
     double highWaterMark;
-    enum class Direction{UP = 1, DOWN = 0};
-    Direction dir;
     void calculate(double nextPrice){
         if(highWaterMark < nextPrice){
             highWaterMark = nextPrice;
         }
-        if(nextPrice - .1 < lastPrice){
-                // sell was going up but now going down....
-                if(dir == Direction::UP){
-                    double lowWaterMark = highWaterMark;
-                    if(percentStop){
-                        lowWaterMark -= highWaterMark * stop;
-                    }else{ // must be dollar value drop
-                        lowWaterMark -= stop;
-                    }
-                    if(nextPrice <= lowWaterMark){
-                        OrderResponse order = factory.getBroker(paper)->placeEquityOrder(symbol,"sell","10","market","day","","");
-                        if(order.id != "-1"){
-                            // order was accepted
-                            parent->setLastPrice(nextPrice);
-                            parent->setOrderId(order.id);
-                            parent->swapToNextState();
-                        // change state to pending order...
-                        }
-                    }
-                }
-            dir = Direction::DOWN;
+        double lowWaterMark = highWaterMark;
+        if(percentStop){
+            lowWaterMark -= highWaterMark * stop;
+        }else{ // must be dollar value drop
+            lowWaterMark -= stop;
         }
-        else if( nextPrice - .1 > lastPrice){
-            dir = Direction::UP;
+        if(nextPrice <= lowWaterMark){
+            OrderResponse order = factory.getBroker(paper)->placeEquityOrder(symbol,"sell","10","market","day","","");
+            if(order.id != "-1"){
+                // order was accepted
+                parent->setLastPrice(nextPrice);
+                parent->setOrderId(order.id);
+                parent->swapToNextState();
+            // change state to pending order...
+            }
         }
         lastPrice = nextPrice;
     }
@@ -53,7 +42,6 @@ class TrailingStopState: public IState{ // my own implementation of trailing sto
     virtual void init(){
         highWaterMark = 0;
         lastPrice = parent->getLastPrice();
-        dir = Direction::DOWN;
     };
     virtual void onData(std::shared_ptr<IStreamData> data){}; // defualt case
     virtual void onData(std::shared_ptr<OtherData> other){};
